16c8a2616686600a4a4b73b3682a8cd5aa65c610
Michael New <mikenew12@gmail.com>
2017-11-26 14:55:46 -0800

    new script converts left to right layout

--- CoreLMtoRMConverter.py
+++ CoreLMtoRMConverter.py
@@ -1,19 +1,58 @@
 import configparser
 
-
 class ConfigParser(configparser.ConfigParser):
-    """Case-sensitive ConfigParser."""
-
     def optionxform(self, opt):
         return opt
-
-    def write(self, file):
-        return super().write(file, space_around_delimiters=False)
 
 keymapper = ConfigParser()
 keymapper.read('KeyMappings.ini')
 
-def convert_LM_key_to_RM(key):
-    return keymapper.get('LeftToRightMappings', key)
+def remap_single_key_value(key):
+    remapped = ""
+    try:
+        remapped = keymapper.get('LeftToRightMappings', key)
+    except configparser.NoOptionError:
+        remapped = key
+        print("no remapping found for " + key)
+    return remapped
 
-print(convert_LM_key_to_RM('W'))
+def convert_left_medium_key_to_right_medium(keystring):
+    remapped = ""
+
+    commasplitkeys = keystring.split(",")
+    commasplitcount = 1
+
+    for cs in commasplitkeys:
+        plussplitkeys = cs.split('+')
+        plussplitcount = 1
+        for ps in plussplitkeys:
+            remapped += remap_single_key_value(ps)
+            if plussplitcount != len(plussplitkeys):
+                remapped += "+"
+            plussplitcount+=1
+
+        if commasplitcount != len(commasplitkeys):
+            remapped += ","
+        commasplitcount+=1
+
+    return remapped
+
+
+def convert_hotkey_file(inputfilename, outputfilename):
+
+    hotkeyfile = ConfigParser()
+    hotkeyfile.read(inputfilename)
+
+    with open (outputfilename, "w") as outputfile:
+        for section in hotkeyfile.sections():
+            outputfile.write("\n[" + section + "]\n")
+
+            for item in hotkeyfile.items(section):
+                if section == "Commands" or section == "Hotkeys":
+                    remapped = convert_left_medium_key_to_right_medium(item[1])
+                    if remapped:
+                        outputfile.write(item[0] + "=" + remapped + "\n")
+                else:
+                    outputfile.write(item[0] + "=" + item[1] + "\n")
+
+convert_hotkey_file('TheCore RLM .SC2Hotkeys', 'TestROutput.SC2Hotkeys')
